c096a40455eb1182bd961d35387d03e822b3b624
Dzonixy <dzonixy@gmail.com>
2023-06-19 17:24:18 +0200

    tests module build and publish

--- unique/unique-client/src/main.rs
+++ unique/unique-client/src/main.rs
@@ -34,23 +34,27 @@ async fn mint_with_context() {
 
     let account = context.gen_account();
     let create_txn = context.create_user_account_by(&mut root_account, &account);
-    let mint_amount = 1_000_000_000;
-    let mint_txn = root_account.sign_with_transaction_builder(
+
+    let mint_amount = 10_000_000;
+    let mint_account_txn = root_account.sign_with_transaction_builder(
         context
             .transaction_factory()
             .mint(account.address(), mint_amount),
     );
 
+    context
+        .commit_block(&vec![create_txn.clone(), mint_account_txn.clone()])
+        .await;
+}
+
+#[tokio::test(flavor = "multi_thread", worker_threads = 2)]
+async fn build_and_publish() {
+    let mut context = new_test_context(current_function_name!(), NodeConfig::default(), false);
+    let mut root_account = context.root_account().await;
+
     let named_addresses = vec![("toycoin".to_string(), root_account.address())];
     let path = PathBuf::from(std::env!("CARGO_MANIFEST_DIR")).join("../toycoin/");
     let payload = TestContext::build_package(path, named_addresses);
-    let publish_txn = context.publish_package(&mut root_account, payload).await;
 
-    context
-        .commit_block(&vec![
-            create_txn.clone(),
-            mint_txn.clone(),
-            publish_txn.clone(),
-        ])
-        .await;
+    context.publish_package(&mut root_account, payload).await;
 }
