c411ab39824a610a3628eb0d826373ae29044d61
Dzonixy <dzonixy@gmail.com>
2023-06-27 16:27:47 +0200

    fixes named addresses

--- unique/unique-client/src/main.rs
+++ unique/unique-client/src/main.rs
@@ -1,12 +1,6 @@
-use aptos_api_test_context::{current_function_name, new_test_context, ApiSpecificConfig};
-use aptos_config::config::NodeConfig;
-use aptos_node::AptosNodeArgs;
-use aptos_sdk::rest_client::Client;
-use clap::Parser;
-
 #[tokio::main]
 async fn main() {
-    AptosNodeArgs::parse().run()
+    // AptosNodeArgs::parse().run()
 }
 
 #[cfg(test)]
@@ -21,9 +15,8 @@ mod tests {
     };
 
     use aptos_sdk::types::account_address::AccountAddress;
-    use sealed_test::prelude::*;
-    use std::{io::Write, path::PathBuf, str::FromStr};
-    use toml::toml;
+    // use sealed_test::prelude::*;
+    use std::{path::PathBuf, str::FromStr};
 
     #[tokio::test(flavor = "multi_thread", worker_threads = 2)]
     async fn client_build() {
@@ -50,7 +43,7 @@ mod tests {
             .await;
     }
 
-    // #[tokio::test(flavor = "multi_thread", worker_threads = 2)]
+    #[tokio::test(flavor = "multi_thread", worker_threads = 8)]
     async fn build_and_publish() {
         // aptos_logger::Logger::new().init();
         let mut context = new_test_context(current_function_name!(), NodeConfig::default(), false);
@@ -60,69 +53,56 @@ mod tests {
             },
         };
 
+        let mut module_account = context.create_account().await;
         let mut root_account = context.root_account().await;
-        let mut module_account = context.gen_account();
 
         let transfer_txn =
             context.account_transfer(&mut root_account, &module_account, 1_000_000_000);
         context.commit_block(&[transfer_txn]).await;
 
-        let module_address_hex = module_account.address().to_hex_literal();
-        let module_name = "unique".to_string();
-
-        let toml_path = PathBuf::from(std::env!("CARGO_MANIFEST_DIR"))
-            .join(&format!("../{}/Move.toml", "toycoin"));
-        let mut file = std::fs::File::create(toml_path.clone()).unwrap();
-        let move_toml = toml! {
-            [package]
-            name = "Toycoin"
-            version = "0.0.0"
-
-            [addresses]
-            std = "0x1"
-            toycoin = module_address_hex
-
-            [dependencies]
-            AptosStdlib = { local = "../../aptos-move/framework/aptos-stdlib" }
-        };
-        file.write_all(move_toml.to_string().as_bytes()).unwrap();
-
-        let named_addresses = vec![(module_name.clone(), module_account.address())];
+        let named_address = "toycoin".to_string();
+        let named_addresses = vec![(named_address.clone(), module_account.address())];
         let path =
             PathBuf::from(std::env!("CARGO_MANIFEST_DIR")).join(&format!("../{}/", "toycoin"));
 
         let payload = TestContext::build_package(path.clone(), named_addresses);
         context.publish_package(&mut module_account, payload).await;
 
-        client
-            .get_account_module(module_account.address(), "unique")
-            .await
-            .unwrap();
+        assert_ne!(
+            client
+                .get_account_module(module_account.address(), "unique")
+                .await
+                .unwrap()
+                .into_inner()
+                .bytecode
+                .0
+                .len(),
+            0
+        );
 
         let compiled_script = std::fs::read(
             PathBuf::from(std::env!("CARGO_MANIFEST_DIR"))
                 .join("../toycoin/build/toycoin/bytecode_scripts/main.mv"),
         )
         .unwrap();
-
         let script_txn =
             root_account.sign_with_transaction_builder(context.transaction_factory().script(
                 Script::new(compiled_script, vec![], vec![TransactionArgument::U64(1)]),
             ));
-
         context.commit_block(&vec![script_txn]).await;
     }
 
-    #[sealed_test(env = [("RUST_MIN_STACK", "10485760")])]
-    fn publish_toycoin_test() {
-        tokio::runtime::Builder::new_multi_thread()
-            .worker_threads(1)
-            .enable_all()
-            .build()
-            .unwrap()
-            .block_on(build_and_publish())
-    }
+    // #[sealed_test(env = [("RUST_MIN_STACK", "1048576000")])]
+    // fn publish_toycoin_test() {
+    //     tokio::runtime::Builder::new_multi_thread()
+    //         .worker_threads(2)
+    //         .enable_all()
+    //         .build()
+    //         .unwrap()
+    //         .block_on(build_and_publish())
+    // }
 
+    #[tokio::test(flavor = "multi_thread", worker_threads = 8)]
     async fn publish_marketplace() {
         let mut context = new_test_context(current_function_name!(), NodeConfig::default(), false);
         let client = match &context.api_specific_config {
@@ -139,20 +119,20 @@ mod tests {
         let payload = TestContext::build_package(path, named_addresses);
         context.publish_package(&mut root_account, payload).await;
 
-        let module = client.get_account_modules(module_address).await.unwrap();
+        // let module = client.get_account_modules(module_address).await.unwrap();
 
         // TODO: test marketplace
     }
 
-    #[sealed_test(env = [("RUST_MIN_STACK", "10485760")])]
-    fn publish_marketplace_test() {
-        tokio::runtime::Builder::new_multi_thread()
-            .worker_threads(1)
-            .enable_all()
-            .build()
-            .unwrap()
-            .block_on(publish_marketplace())
-    }
+    // #[sealed_test(env = [("RUST_MIN_STACK", "10485760")])]
+    // fn publish_marketplace_test() {
+    //     tokio::runtime::Builder::new_multi_thread()
+    //         .worker_threads(1)
+    //         .enable_all()
+    //         .build()
+    //         .unwrap()
+    //         .block_on(publish_marketplace())
+    // }
 
     #[tokio::test]
     async fn compile_script_from_string() {
